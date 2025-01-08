c9a2b2626e96e94ffe7c82fce9b9ebc5cca0676a
Pantareje <62074882+Pantareje@users.noreply.github.com>
2025-01-08 19:07:44 +0100

    Manejado el no especificar una tarea.

--- src/Main.cpp
+++ src/Main.cpp
@@ -5,10 +5,10 @@
 #include <format>
 
 namespace {
-    class ArgumentError final : public std::runtime_error {
+    class ArgumentException final : public std::runtime_error {
     public:
-        explicit ArgumentError(const char* message) : std::runtime_error(message) {}
-        explicit ArgumentError(const std::string& message) : std::runtime_error(message) {}
+        explicit ArgumentException(const char* message) : std::runtime_error(message) {}
+        explicit ArgumentException(const std::string& message) : std::runtime_error(message) {}
     };
     
     class ArgumentsReader {
@@ -42,37 +42,41 @@ namespace {
             while (GetNextArgument()) {
                 if (m_arg == "-t") {
                     if (attributes.taskType != TaskType::None)
-                        throw ArgumentError("Ya se ha indicado un tipo de tarea a realizar.");
+                        throw ArgumentException("Ya se ha indicado un tipo de tarea a realizar.");
                     attributes.taskType = TaskType::Tokens;
                 } else if (m_arg == "-s") {
                     if (attributes.taskType != TaskType::None)
-                        throw ArgumentError("Ya se ha indicado un tipo de tarea a realizar.");
+                        throw ArgumentException("Ya se ha indicado un tipo de tarea a realizar.");
                     attributes.taskType = TaskType::Symbols;
                 } else if (m_arg == "-p") {
                     if (attributes.taskType != TaskType::None)
-                        throw ArgumentError("Ya se ha indicado un tipo de tarea a realizar.");
+                        throw ArgumentException("Ya se ha indicado un tipo de tarea a realizar.");
                     attributes.taskType = TaskType::Parse;
                 } else if (m_arg == "-u") {
                     attributes.useSemantic = true;
                 } else if (m_arg == "-i") {
                     if (!attributes.inputFileName.empty())
-                        throw ArgumentError("Ya se ha definido un fichero de entrada.");
+                        throw ArgumentException("Ya se ha definido un fichero de entrada.");
                     if (!GetNextArgument())
-                        throw ArgumentError("Después de «-i» se debe especificar un fichero de entrada.");
+                        throw ArgumentException("Después de «-i» se debe especificar un fichero de entrada.");
                     if (m_arg.empty())
-                        throw ArgumentError("El nombre del fichero de entrada no puede estar vacío.");
+                        throw ArgumentException("El nombre del fichero de entrada no puede estar vacío.");
                     attributes.inputFileName = m_arg;
                 } else if (m_arg == "-o") {
                     if (!attributes.outputFileName.empty())
-                        throw ArgumentError("Ya se ha definido un fichero de salida.");
+                        throw ArgumentException("Ya se ha definido un fichero de salida.");
                     if (!GetNextArgument())
-                        throw ArgumentError("Después de «-o» se debe especificar un fichero de salida.");
+                        throw ArgumentException("Después de «-o» se debe especificar un fichero de salida.");
                     if (m_arg.empty())
-                        throw ArgumentError("El nombre del fichero de salida no puede estar vacío.");
+                        throw ArgumentException("El nombre del fichero de salida no puede estar vacío.");
                     attributes.outputFileName = m_arg;
                 } else {
-                    throw ArgumentError(std::format("El argumento «{}» es desconocido.", m_arg));
+                    throw ArgumentException(std::format("El argumento «{}» es desconocido.", m_arg));
                 }
+            }
+
+            if (attributes.taskType == TaskType::None) {
+                throw ArgumentException("Es necesario indicar una tarea a relizar.");
             }
 
             return attributes;
@@ -85,7 +89,7 @@ int main(const int argc, const char* argv[]) {
         const ApplicationAttributes attributes = ArgumentsReader(argc, argv).GetAttributes();
         const Application application(attributes);
         return application.Run();
-    } catch (const ArgumentError& e) {
+    } catch (const ArgumentException& e) {
         std::cerr << e.what() << std::endl;
         return 4;
     }
